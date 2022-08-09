fe41a76b04cd200173769dcbc830ead679e2a208
ThiagoGarcia <thiagoncg1@gmail.com>
2022-08-09 16:14:39 -0300

    acelerometro
    acelerometro

---
+++ ProgramacaoIV/Assets/Scripts/Acelerometre.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acelerometre : MonoBehaviour
+{
+    private float speed  = 10.0f;
+
+    void Update()
+    {
+        //IntroductionAcer();
+        MoveObject();
+    }
+
+    private void IntroductionAcer()
+    {
+        transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+        Debug.Log(Input.acceleration.x);
+        Debug.Log(Input.acceleration.y);
+        Debug.Log(Input.acceleration.z);
+    }
+
+    private void MoveObject()
+    {
+        Vector3 direction = Vector3.zero;
+
+
+        // Brinque com estes valores para entender a orientação o dispositivo
+        direction.x = -Input.acceleration.y;
+        direction.z = Input.acceleration.x;
+
+        if(direction.sqrMagnitude > 1)
+        {
+            direction.Normalize();
+        }
+
+        direction *= Time.deltaTime;
+
+        transform.Translate (direction * speed);
+    }
+}
