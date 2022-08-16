6b02a760309e0063389315f5d42e36d5fac7d3fe
ThiagoGarcia <thiagoncg1@gmail.com>
2022-08-16 18:03:18 -0300

    Comentário do analógico
    Comentário do analógico

--- ProgramacaoIV/Assets/Scripts/analogic.cs
+++ ProgramacaoIV/Assets/Scripts/analogic.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class analogic : MonoBehaviour
 {
-    private Touch touchDevice;
+    private Touch touchDevice; // responsável pelo toque na tela
     private Vector2 startPos;
     public Transform background;
     public float radius = 150.0f;
@@ -12,21 +12,21 @@ public class analogic : MonoBehaviour
     public float speedPlayer = 0.2f;
     void Start()
     {
-        touchDevice = new Touch { fingerId = -1 };
+        touchDevice = new Touch { fingerId = -1 }; // o toque vai inicializar com um id
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        startPos = background.position;
 
-        if (Input.touchCount > 0)
+        if (Input.touchCount > 0) // verifica se tem toque na tela
         {
-            for (int i = 0; i < Input.touchCount; i++)
+            for (int i = 0; i < Input.touchCount; i++) // percorre por todos os inputs toques
             {
-                if (touchDevice.fingerId == -1)
+                if (touchDevice.fingerId == -1) // verifica se a variavel touch foi atribuida
                 {
+                    // verifica em que quadrante esta o toque na tela
                     if (Input.GetTouch(i).position.x < Screen.width / 2 && Input.GetTouch(i).position.y < Screen.height / 2)
                     {
                         touchDevice = Input.GetTouch(i);
@@ -36,26 +36,26 @@ public class analogic : MonoBehaviour
                 }
                 else
                 {
-                    if (Input.GetTouch(i).fingerId == touchDevice.fingerId)
+                    if (Input.GetTouch(i).fingerId == touchDevice.fingerId) // verifica a atualização do toque na tela
                     {
                         touchDevice = Input.GetTouch(i);
                     }
                 }
             }
 
-            if (touchDevice.fingerId != -1)
+            if (touchDevice.fingerId != -1) // verifica se o toque foi cancela ou terminado. se isso ocorreu tudo deve ser zerado
             {
                 if(touchDevice.phase == TouchPhase.Canceled || touchDevice.phase == TouchPhase.Ended) 
                 {
                     touchDevice = new Touch { fingerId = -1 };
                 }
-                else
+                else// se não foi cancelado movimentamos o player
                 {
                     Vector2 distance = touchDevice.position - startPos;
 
-                    transform.position = startPos + Vector2.ClampMagnitude(distance, radius);
+                    transform.position = startPos + Vector2.ClampMagnitude(distance, radius); // cria uma barreira do analogico determinado pelo radiuns
 
-                    Player.position += (Vector3)distance * speedPlayer * Time.deltaTime;
+                    Player.position += (Vector3)distance * speedPlayer * Time.deltaTime; // movimenta o player de acordo com a velocidade
 
                     Player.up = Player.position + (Vector3)distance;
                 }
